c3aab8a1973bdc3dc238fd3dac73a7faadafcde8
osama-afifi <osama.egt@gmail.com>
2013-12-13 00:19:09 +0200

    created decrypt.cs

--- ImageHiding/ImageHiding/Decrypt.cs
+++ ImageHiding/ImageHiding/Decrypt.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using BitmapProcessing;
 
 namespace ImageHiding
 {
     class Decrypt
     {
+        private string secretMessage;
+        private Bitmap stegoImageBitmap;
+        private FastBitmap stegoImage;
 
          public Decrypt(string secretMessage, string coverImageDirectory)
         {
             this.secretMessage = secretMessage;
-            coverImageBitmap = new Bitmap(coverImageDirectory); // Creating Ordinary Bitmap to Load Image from Path 
-            coverImage = new FastBitmap(coverImageBitmap);  // FastBitmap is a costum created unsafe bitmap which allow faster access by manual locking/unlocking
+            stegoImageBitmap = new Bitmap(coverImageDirectory); // Creating Ordinary Bitmap to Load Image from Path 
+            stegoImage = new FastBitmap(stegoImageBitmap);  // FastBitmap is a costum created unsafe bitmap which allow faster access by manual locking/unlocking
 
         }
 
--- ImageHiding/ImageHiding/Encrypt.cs
+++ ImageHiding/ImageHiding/Encrypt.cs
@@ -21,6 +21,27 @@ namespace ImageHiding
             coverImage = new FastBitmap(coverImageBitmap);  // FastBitmap is a costum created unsafe bitmap which allow faster access by manual locking/unlocking
 
         }
+
+        public void Run()
+        {
+            coverImage.LockImage(); // Lock Cover Image for Accessing
+
+            // partitionMessage();
+            // generateRandomSequence();
+            // HashRecurrence();
+            //ReplacePixels();
+
+            ///////////////////////////////////////////////////
+            //  3shan tst3ml el pixel get w set shofo da 3shan 2na mnzl class lil bitmap m5soos 2sr3 mn el default f lasm t2ro da
+            // http://www.vcskicks.com/fast-image-processing2.php
+            // lw 5lsna el klam da n3ml class el Decrypt w yb2a 5lsna mn el project mn 8er optimization
+            // ya ret n5ls da 2nhrda
+            ////////////////////////////////////////
+            coverImage.UnlockImage();
+            coverImageBitmap.Dispose(); // Clear Image from Memory
+
+        }
+
         static string hashFunction(int a, int b, int c, int d, int l)
         {
             List<int> Nums = new List<int>();
@@ -63,25 +84,5 @@ namespace ImageHiding
             }
             return param;
         }
-        public void Run()
-        {
-            coverImage.LockImage(); // Lock Cover Image for Accessing
-
-            // partitionMessage();
-            // generateRandomSequence();
-            // HashRecurrence();
-            //ReplacePixels();
-
-            ///////////////////////////////////////////////////
-            //  3shan tst3ml el pixel get w set shofo da 3shan 2na mnzl class lil bitmap m5soos 2sr3 mn el default f lasm t2ro da
-            // http://www.vcskicks.com/fast-image-processing2.php
-            // lw 5lsna el klam da n3ml class el Decrypt w yb2a 5lsna mn el project mn 8er optimization
-            // ya ret n5ls da 2nhrda
-            ////////////////////////////////////////
-            coverImage.UnlockImage();
-            coverImageBitmap.Dispose(); // Clear Image from Memory
-        
-        }
-
     }
 }
