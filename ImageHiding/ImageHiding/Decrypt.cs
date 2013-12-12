797bd9af9ac825511c78612e931234c8b947303e
osama-afifi <osama.egt@gmail.com>
2013-12-13 00:16:13 +0200

    added decrypt.cs

---
+++ ImageHiding/ImageHiding/Decrypt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageHiding
+{
+    class Decrypt
+    {
+
+         public Decrypt(string secretMessage, string coverImageDirectory)
+        {
+            this.secretMessage = secretMessage;
+            coverImageBitmap = new Bitmap(coverImageDirectory); // Creating Ordinary Bitmap to Load Image from Path 
+            coverImage = new FastBitmap(coverImageBitmap);  // FastBitmap is a costum created unsafe bitmap which allow faster access by manual locking/unlocking
+
+        }
+
+
+    }
+}
