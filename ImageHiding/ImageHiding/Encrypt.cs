a5017dca6ba499ae859d3a0abebd9fd6ad9a3c52
osama-afifi <osama.egt@gmail.com>
2013-12-12 14:15:31 +0200

    task's descryption

--- ImageHiding/ImageHiding/Encrypt.cs
+++ ImageHiding/ImageHiding/Encrypt.cs
@@ -13,7 +13,7 @@ namespace ImageHiding
         private string secretMessage;
         private Bitmap coverImageBitmap;
         private FastBitmap coverImage;
-        Encrypt(string secretMessage, string coverImageDirectory)
+        public Encrypt(string secretMessage, string coverImageDirectory)
         {
             this.secretMessage = secretMessage;
             coverImageBitmap = new Bitmap(coverImageDirectory); // Creating Ordinary Bitmap to Load Image from Path 
@@ -29,6 +29,13 @@ namespace ImageHiding
             // generateRandomSequence();
             // HashRecurrence();
             //ReplacePixels();
+
+            ///////////////////////////////////////////////////
+            //  3shan tst3ml el pixel get w set shofo da 3shan 2na mnzl class lil bitmap m5soos 2sr3 mn el default f lasm t2ro da
+            // http://www.vcskicks.com/fast-image-processing2.php
+            // lw 5lsna el klam da n3ml class el Decrypt w yb2a 5lsna mn el project mn 8er optimization
+            // ya ret n5ls da 2nhrda
+            ////////////////////////////////////////
 
             coverImage.UnlockImage();
             coverImageBitmap.Dispose(); // Clear Image from Memory
