4283a10876583d3bf2671fd1cbccd024226bb461
osama-afifi <osama.egt@gmail.com>
2013-12-13 03:49:18 +0200

    check encrypt.cs

--- ImageHiding/ImageHiding/Encrypt.cs
+++ ImageHiding/ImageHiding/Encrypt.cs
@@ -14,22 +14,26 @@ namespace ImageHiding
         private string secretMessage;
         private Bitmap coverImageBitmap;
         private FastBitmap coverImage;
+        BitArray MessageBitString;
+        private int NumberOfLSB;
+        int BitPartitions;
 
         public Encrypt(string secretMessage, string coverImageDirectory)
         {
             this.secretMessage = secretMessage;
             coverImageBitmap = new Bitmap(coverImageDirectory); // Creating Ordinary Bitmap to Load Image from Path 
             coverImage = new FastBitmap(coverImageBitmap);  // FastBitmap is a costum created unsafe bitmap which allow faster access by manual locking/unlocking
-
+            NumberOfLSB = 4;
+            BitPartitions =  (secretMessage.Length+NumberOfLSB-1)/NumberOfLSB;
         }
 
-        public string Run(int NumberOfLSB = 4)
+        public string Run()
         {
-            BitArray MessageBitString = new BitArray(PartitionMessage(secretMessage));
+            MessageBitString = new BitArray(PartitionMessage(secretMessage));
             string outputHash = "";
             int x0 = 0, a = 1, b = 1, c = 1;// x0 , xi+1 = a(xi+1)^b +c passed by reference
             GenerateSequence(ref x0, ref a, ref b, ref c);
-            ReplacePixels(ref coverImage);
+            ReplacePixels(x0,a,b,c);
             outputHash = HashRecurrence(x0, a, b, c, secretMessage.Length);
             return outputHash;
         }
@@ -53,14 +57,21 @@ namespace ImageHiding
             }
             return hashed;
         }
-        private BitArray PartitionMessage(string secretMessage)
+        private byte[] PartitionMessage(string secretMessage)
         {
             int len = secretMessage.Length;
-            BitArray bitString = new BitArray(len * 8 + 1);
-            for (int i = 0; i < len; i++)
-                for (int bit = 0; bit < 8; bit++)
-                    bitString.Set(i * 8 + bit, (secretMessage[i] & (1 << (7 - bit))) == 1);
+            int partSize = (8+NumberOfLSB-1)/NumberOfLSB;
+            byte []bitString = new byte[len * 8 + 1];
+            
 
+            for (int i = 0; i < len; i++)
+                for (int part = 0; part < partSize ; i++)
+                {
+                    int c = 0;
+                    for (int bit = 0; bit <itions; bit++)
+                        bitString[i * BitPartitions + part]  = (byte)(((int)secretMessage[i] & (1 << (7 - bit))));
+                }
+                    
             return bitString;
         }
 
@@ -70,15 +81,63 @@ namespace ImageHiding
             // for now x0 = 0 , xi+1 = (1xi^1+1)%(m*n)
         }
 
-        private Bitmap ReplacePixels(ref FastBitmap originalImage)
+        private void ReplacePixels(int x0,int a,int b,int c)
         {
-            Bitmap stegoImage = new Bitmap(
-            coverImage.LockImage();
-            
-            coverImage.UnlockImage();
-            coverImageBitmap.Dispose(); // Clear Image from Memory
+            Bitmap stegoImage = new Bitmap(coverImageBitmap);
+            int n = stegoImage.Height;
+            int m = stegoImage.Width;
+            int index = x0;
+            int counter = 0;
+            for(int i = 0 ;i<MessageBitString.Length ; i++)
+            { 
+                int x = index/n;
+                int y = index%m;
+                while()
+                byte newLSB = MessageBitString
+                Color OldColor = stegoImage.GetPixel(x,y);
+                Color NewColor = Color.FromArgb( ReplaceLSB(OldColor.ToArgb(), ))
+
+                
+                Color p =         
+                    counter = 0;
+                    index = a
+            }
+            //coverImage.LockImage();
+            //coverImage.UnlockImage();
+            //coverImageBitmap.Dispose(); // Clear Image from Memory
             
         }
+
+        private int powerMod(ref int Number, int Power , ref int MOD)
+        {
+            if (Power == 0) return 1;
+            
+
+            if (Power % 2 == 1)
+               return (powerMod(ref Number, Power-1 , ref MOD)%MOD * Number%MOD)%MOD;
+            
+            else
+            {
+                int Ret = powerMod(ref Number, Power-1 , ref MOD);
+                return ((Ret%MOD)*(Ret%MOD))%MOD;
+            }
+            
+        }
+        
+        private int ReplaceLSB(int original , byte pat)
+        {
+            int ret = original;
+            ret = ((ret & ( ~((1<<(NumberOfLSB + 1))-1)))); // clear k bits
+            ret |= ((int)pat & ((1 << (NumberOfLSB + 1)) - 1)); // set the k bits
+            return ret;
+        }
+}
+
+
+
+
+
+
 
     }
 }
