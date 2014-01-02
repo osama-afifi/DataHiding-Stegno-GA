1414d82ce1605aac6953154cee35bea7630621d7
osama-afifi <osama.egt@gmail.com>
2014-01-02 22:58:05 +0200

    added Organism Class implents Icomparable to sort for it's fitness
    added Population Handlers functions include
    createInitialPopulation
    rankPopulation
    matePopulation
    
    added Mating Operators include
    crossover
    mutate
    elitism
    
    Selection algorithm section is still empty

--- ImageHiding/ImageHiding/GeneticAlgorithm/GeneticAlgorithm.cs
+++ ImageHiding/ImageHiding/GeneticAlgorithm/GeneticAlgorithm.cs
@@ -7,50 +7,178 @@ using System.Threading.Tasks;
 
 namespace ImageHiding.GeneticAlgorithm
 {
+
     class GeneticAlgorithm
     {
-        public int populationSize;
-        public int numberOfGenerations;
-        public double crossoverRate;
-        public double mutationRate;
-        public int elitismFactor;
-        public bool printMode;
-        public delegate double EvaluationDelegate(params int[] values);        
-        EvaluationDelegate fitnessFunction;
+
+        public enum SelectionMode
+        {
+            RouletteWheel,
+            Tournament,
+            RewardBased
+        };
+
+        #region private members
+        SelectionMode Selection;
+        private static Random GARandomGenerator = new Random();
+        private int populationSize;
+        private int numberOfGenerations;
+        private double crossoverRate;
+        private double mutationRate;
+        private int chromosomeLength;
+        private int elitismFactor;
+        private ArrayList population;
+        private KeyValuePair<int, int>[] genesDomain;
+        #endregion
+
+        public delegate double EvaluationDelegate(params int[] values);
+        public EvaluationDelegate fitnessFunction;
+        public KeyValuePair<int, int>[] GenesDomain
+        {
+            set
+            {
+                genesDomain = value;
+            }
+
+            get
+            {
+                return genesDomain;
+            }
+        }
         public EvaluationDelegate FitnessFunction
         {
             get
             {
                 return fitnessFunction;
             }
-            set 
+            set
             {
-                fitnessFunction = value; 
+                fitnessFunction = value;
             }
         }
-        static Random GARandomGenerator = new Random();
+        public bool PrintLogMode;
 
-        public GeneticAlgorithm(int populationSize, int numberOfGenerations, double crossoverRate, double mutationRate, EvaluationDelegate fitnessFunction, int elitismFactor = 0)
+
+        public GeneticAlgorithm(int populationSize, int chromosomeLength, int numberOfGenerations, double crossoverRate, double mutationRate, EvaluationDelegate fitnessFunction, int elitismFactor = 0, SelectionMode Selection = SelectionMode.RouletteWheel , bool PrintLogMode = false)
         {
             this.populationSize = populationSize;
+            this.chromosomeLength = chromosomeLength;
             this.numberOfGenerations = numberOfGenerations;
             this.crossoverRate = crossoverRate;
             this.mutationRate = mutationRate;
-            this.elitismFactor = elitismFactor;
             this.fitnessFunction = fitnessFunction;
+            this.elitismFactor = elitismFactor;
+            this.PrintLogMode = PrintLogMode;
+            genesDomain = new KeyValuePair<int, int>[chromosomeLength];
         }
 
-        public void runSimulation()
-        { 
-            //create initial organsims
-            //rank organisms
-            //generate new population
-            // select two different organism using a Roulette Selection
-            // do crossover || mutation || elitism
-            // repeat till satisfied
-            // return best
+        public Organism Run() // Warning you should assign an Fitness Function and genes Domain before running
+        {
+            createInitialPopulation();
+            for (int curGeneration = 1; curGeneration <= numberOfGenerations; ++curGeneration)
+            {
+                rankPopulation();
+                population.Sort();
+                matePopulation();
+            }
+            Organism Best = (Organism)population[0]; // return the best Organism as Optimal Solution
+            return Best;
         }
 
+        #region population handlers
+        private void createInitialPopulation()
+        {
+            population.Clear();
+            for (int i = 0; i < populationSize; i++)
+            {
+                Organism genome = new Organism(chromosomeLength, ref genesDomain);
+                population.Add((Organism)genome);
+            }
+        }
+        private void rankPopulation()
+        {
+            for (int i = 0; i < populationSize; i++)
+            {
+                Organism genome = (Organism)population[i];
+                genome.fitnessValue = FitnessFunction(genome.chromosome);
+            }
+            //population.Sort();
+        }
+        private void matePopulation()
+        {
+            ArrayList newPopulation = new ArrayList();
+            // Save the Elites
+            ArrayList Elites = new ArrayList();
+            for (int i = 0; i < elitismFactor; i++)
+                Elites.Add((Organism)population[i]);
+            for (int i = 0; i < populationSize; i++)
+            {
+                Organism parent1 = (Organism)population[ParentSelection()];
+                Organism parent2 = (Organism)population[ParentSelection()];
+                Organism child1 = new Organism();
+                Organism child2 = new Organism();                   
+                if (GARandomGenerator.NextDouble() < crossoverRate)
+                    crossover(ref parent1, ref parent2, out child1, out child2);
+                mutate(ref child1);
+                mutate(ref child2);
+                newPopulation.Add((Organism)child1);
+                newPopulation.Add((Organism)child2);
+            }
+            for (int i = 0; i < elitismFactor && i<populationSize; i++)
+                newPopulation[i] = (Organism)Elites[i];
+            population.Clear();
+            population = new ArrayList(newPopulation);
+        }
+        #endregion
+
+        #region Genetic Operators
+
+        private int ParentSelection()
+        {
+            int parentIndex = 0;
+            switch (Selection)
+            {
+                case SelectionMode.RouletteWheel:
+                    parentIndex = RouletteWheel();
+                    break;
+                case SelectionMode.Tournament:
+                    parentIndex = Tournament();
+                    break;
+                case SelectionMode.RewardBased:
+                    parentIndex = RewardBased();
+                    break;
+            }
+            return parentIndex;
+        }
+        private void crossover(ref Organism parent1, ref Organism parent2, out Organism child1, out Organism child2)
+        {
+            int pivot = GARandomGenerator.Next(0, chromosomeLength - 1);
+            for (int i = pivot; i < chromosomeLength; i++)
+            {
+                int temp = parent1.chromosome[i];
+                parent1.chromosome[i] = parent2.chromosome[i];
+                parent2.chromosome[i] = temp;
+            }
+            child1 = parent1;
+            child2 = parent2;
+        }
+
+        private void mutate(ref Organism mutatedOrganism)
+        {
+            for (int i = 0; i < chromosomeLength; i++)
+                if (GARandomGenerator.NextDouble() < mutationRate)
+                    mutatedOrganism.chromosome[i] = (mutatedOrganism.chromosome[i] + GARandomGenerator.Next(genesDomain[i].Key, genesDomain[i].Value)) / 2;
+        }
+        #endregion
+
+        #   region Selection Algorithms
+        private int RouletteWheel()
+        { return -1; }
+        private int Tournament()
+        { return -1; }
+        private int RewardBased()
+        { return -1; }
+        #   endregion
 
     }
 }
--- ImageHiding/ImageHiding/GeneticAlgorithm/Organism.cs
+++ ImageHiding/ImageHiding/GeneticAlgorithm/Organism.cs
@@ -6,7 +6,85 @@ using System.Threading.Tasks;
 
 namespace ImageHiding.GeneticAlgorithm
 {
-    class Organism
+    
+    class Organism : IComparable<Organism> , IEquatable<Organism>
     {
+       // public static int chromosomeLength;
+        static Random RandomGenerator = new Random();
+        public int[] chromosome;
+        public double fitnessValue;
+
+        public Organism() { }
+        public Organism(int chromosomeLength, ref KeyValuePair<int, int>[] genesDomain)
+        {
+            chromosome = new int[chromosomeLength];
+            for (int i = 0; i < chromosomeLength; i++)
+                chromosome[i] = RandomGenerator.Next(genesDomain[i].Key, genesDomain[i].Value); // set the bounds of the gene
+        }
+        
+
+
+        # region IEquatable
+        public bool Equals(Organism other)
+        {
+            if (other == null)
+                return false;
+            if (this.fitnessValue == other.fitnessValue)
+                return true;
+            else
+                return false;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            Organism p = obj as Organism;
+            if (p == null)
+                return false;
+            else
+                return Equals(p);
+        }
+
+        public static bool operator ==(Organism p1, Organism p2)
+        {
+            if ((object)p1 == null || ((object)p2) == null)
+                return object.Equals(p1, p2);
+
+            return p1.Equals(p2);
+        }
+
+        public static bool operator !=(Organism p1, Organism p2)
+        {
+            if (p1 == null || p2 == null)
+                return !object.Equals(p1, p2);
+
+            return !(p1.Equals(p2));
+        }
+
+        # endregion
+
+        # region IComparable
+        public int CompareTo(Organism x)
+        {
+            if (fitnessValue < x.fitnessValue)
+                return -1;
+            else if (fitnessValue > x.fitnessValue)
+                return 1;
+            else
+                return 0;
+        }
+
+        public static bool operator <(Organism p1, Organism p2)
+        {
+            return p1.CompareTo(p2) < 0;
+        }
+
+        public static bool operator >(Organism p1, Organism p2)
+        {
+            return p1.CompareTo(p2) > 0;
+        }
+        # endregion
     }
 }
